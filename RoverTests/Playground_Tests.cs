0a242d3199b95d9c8825d5da751e50f9706e7323
4uva <41539104+4uva@users.noreply.github.com>
2019-06-16 23:09:09 +0300

    added playground class and tests

---
+++ RoverTests/Playground_Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rover_ConsoleApp;
+
+
+namespace RoverTests
+{ [TestClass]
+    class Playground_Tests
+    {
+        [TestMethod]
+        public bool IsPointInside()
+            {
+            PlayGround playground = new PlayGround(0, 0, 3, 3 );
+            playground.IsPointInside();
+                Assert.AreEqual(1, 1, true);
+
+           
+        }
+            //        пример теста
+            //1) мы создаём Playground с границами xmin = 0, ymin = 0, xmax = 3, ymax = 3, спрашиваем, внутри ли точка(1, 1), ожидаемый ответ — да
+            //2) мы создаём Playground с границами xmin = 0, ymin = 0, xmax = 3, ymax = 3, спрашиваем, внутри ли точка(0, 3), ожидаемый ответ — да
+            //3) мы создаём Playground с границами xmin = 0, ymin = 0, xmax = 3, ymax = 3, спрашиваем, внутри ли точка(-1, 2), ожидаемый ответ — нет
+            //4) мы создаём Playground с границами xmin = 0, ymin = 0, xmax = 3, ymax = 3, спрашиваем, внутри ли точка(2, 4), ожидаемый ответ — нет
+            //5) мы создаём Playground с границами xmin = 0, ymin = 0, xmax = 3, ymax = 3, спрашиваем, внутри ли точка(5, -1), ожидаемый ответ — нет
+    }
+}
---
+++ VehicleModel/PlayGround.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rover_ConsoleApp
+{
+    public class PlayGround
+    {
+        int xCoordinateMin;
+        int xCoordinateMax;
+        int yCoordinateMin;
+        int yCoordinateMax;
+
+
+
+        public PlayGround(int xCoordinateMin, int xCoordinateMax, int yCoordinateMin,
+        int yCoordinateMax)
+        {
+            this.xCoordinateMin = xCoordinateMin;
+            this.yCoordinateMin = yCoordinateMin;
+            this.xCoordinateMax = xCoordinateMax;
+            this.yCoordinateMin = yCoordinateMin;
+        }
+        public bool IsPointInside(int xCoordinate, int yCoordinate)
+        {
+            if (xCoordinate > xCoordinateMin && yCoordinate > yCoordinateMin)
+            {
+                if (xCoordinate < xCoordinateMax && yCoordinate < yCoordinateMax) 
+                return true;
+                else { return false; }
+            }
+           else { return false; } 
+        }
+    }
+}
+
+
